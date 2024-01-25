224578fea8c75782bc10401abd63d3a5f032f432
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-25 16:46:56 -0300

    chore: cores configuradas

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,25 @@
- 
+const colors = {
+  'green-primary': '#00875F',
+  'green-secondary': '#00B37E',
+  'gray-primary': '#E1E1E6',
+  'gray-secondary': '#C4C4CC',
+  'gray-terciary': '#7C7C8A',
+  'gray-fourth': '#323238',
+  'gray-fifth': '#29292E',
+  'gray-sixth': '#202024',
+  'gray-seventh': '#121214',
+  'red-primary': '#F75A68',
+};
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-    extend: {},
+  content: ['./App.tsx', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      textColor: colors,
+      backgroundColor: colors,
+      borderColor: colors,
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
