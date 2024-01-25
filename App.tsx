8607fd57a0fe6846dd856828eeeb307c3873ab37
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-25 16:44:59 -0300

    feat: Loading e SignIn adicionado

--- App.tsx
+++ App.tsx
@@ -1,17 +1,23 @@
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 import { StatusBar } from 'expo-status-bar';
 
 import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 
+import { Loading } from '@components/index';
+
+import { SignIn } from '@screens/SignIn';
+
 export default function App() {
   const [fontLoaded] = useFonts({ Inter_400Regular, Inter_700Bold });
 
-  if (!fontLoaded) return;
+  if (!fontLoaded) return <Loading />;
 
   return (
     <View className="flex-1 justify-center items-center">
       <StatusBar style="light" backgroundColor="transparent" translucent />
+
+      <SignIn />
     </View>
   );
 }
