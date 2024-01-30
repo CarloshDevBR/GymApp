386b13dcf37a9a3a082cc42717914f3e77fa8669
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-30 11:11:35 -0300

    chore: mock dos exercicios criado

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,3 +4,12 @@ export const GROUPS = [
   { id: '3', name: 'Tríceps' },
   { id: '4', name: 'Ombro' },
 ];
+
+export const EXERCISES = [
+  { id: '1', image: '', name: 'Puxada frontal', description: '3 séries x 12 repetições' },
+  { id: '2', image: '', name: 'Puxada frontal', description: '3 séries x 12 repetições' },
+  { id: '3', image: '', name: 'Puxada frontal', description: '3 séries x 12 repetições' },
+  { id: '4', image: '', name: 'Puxada frontal', description: '3 séries x 12 repetições' },
+  { id: '5', image: '', name: 'Puxada frontal', description: '3 séries x 12 repetições' },
+  { id: '6', image: '', name: 'Puxada frontal', description: '3 séries x 12 repetições' },
+];
